6ee004ecb3ef4453bc51a02a4d9ad693744876f0
abenjmaa <benjmaa.abdelaziz@gmail.com>
2019-05-30 18:00:13 +0100

    add Nav Bar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-  handleIncrement = produit => {
-    console.log(produit);
-    this.setState({ value: this.state.value + 1 });
-  };
   render() {
     return (
       <div>
         <span className={this.getBadgeClass()}>{this.formatCounter()}</span>
         <button
           className="btn btn-secondary btn-sm"
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
         >
           Increment
         </button>
@@ -30,12 +23,12 @@ class Counter extends Component {
 
   getBadgeClass() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCounter() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
