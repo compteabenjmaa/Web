c90998b624f475bda57e9f82330f7974d8cb9b66
abenjmaa <benjmaa.abdelaziz@gmail.com>
2020-04-04 13:48:49 +0100

    build interface

--- client-app-1/src/components/Person.jsx
+++ client-app-1/src/components/Person.jsx
@@ -8,20 +8,58 @@ class Person extends Component {
     persons: [],
   };
   render() {
-    return this.getTab();
+    return (
+      <div>
+        <span>{this.getForm()}</span>
+        <span> {this.getTab()} </span>
+      </div>
+    );
+  }
+
+  getForm() {
+    return (
+      <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="input"
+              className="form-control"
+              id="name"
+              aria-describedby="emailHelp"
+              placeholder="Enter name"
+            ></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="age">Age</label>
+            <input
+              type="input"
+              className="form-control"
+              id="age"
+              aria-describedby="emailHelp"
+              placeholder="Enter age"
+            ></input>
+          </div>
+          <button type="submit" className="btn btn-primary" value="Envoyé">
+            Envoyer
+          </button>
+        </form>
+      </React.Fragment>
+    );
   }
 
   getTab() {
     if (this.state.persons.length === 0) {
-      return <div className="alert alert-primary">pas d'éléments</div>;
+      return <div className="alert alert-primary mt-2">pas d'éléments</div>;
     } else {
       return (
         <React.Fragment>
-          <table className="table table-striped table-bordered">
+          <table className="table table-striped table-bordered mt-2">
             <thead>
               <tr>
                 <th>Name</th>
                 <th>Age</th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody>
@@ -29,6 +67,29 @@ class Person extends Component {
                 <tr key={p.id}>
                   <td>{p.name}</td>
                   <td>{p.age}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-sm mr-2 w-25"
+                    >
+                      <span
+                        className="glyphicon glyphicon-edit"
+                        aria-hidden="true"
+                      ></span>
+                      Edit
+                    </button>
+
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-sm w-25"
+                    >
+                      <span
+                        className="glyphicon glyphicon-edit"
+                        aria-hidden="true"
+                      ></span>
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
