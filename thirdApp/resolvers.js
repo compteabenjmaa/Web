45b902ab868af0bd59aeb68b5417fc700ec8c82f
abenjmaa <benjmaa.abdelaziz@gmail.com>
2020-04-07 17:47:49 +0100

    add query with param

--- thirdApp/resolvers.js
+++ thirdApp/resolvers.js
@@ -1,13 +1,17 @@
 const db = require('./db')
 const Query = {
    greeting: () => 'Test Success, GraphQL server is up & running !!',
-   student:() => db.students.list()
+   student:() => db.students.list(),
+   studentById:(root,args,context,info)=> {
+     return  db.students.get(args.id);
+   }
 }
 
 const Student = {
   college: (root) => {
 	  return db.colleges.get(root.collegeId);
   }
+ 
 }
 
 module.exports = {Query , Student}
